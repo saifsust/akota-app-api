f930bc61a46b7bb4893fca7f1e7cd898c2758731
saifsust <saiful.sust.cse@gmail.com>
2019-04-29 01:54:12 +0600

    file upload and image move,delete impl

---
+++ src/main/java/com/hungry/services/util/ConsumeType.java
@@ -0,0 +1,8 @@
+package com.hungry.services.util;
+
+public interface ConsumeType {
+
+	public static final String JOSN = "application/json";
+	public static final String MULTIPART_FORM_DATA = "multipart/form-data";
+
+}
