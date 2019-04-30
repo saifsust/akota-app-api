933a9a86ae4166896747f0842273c8283133144a
saifsust <saiful.sust.cse@gmail.com>
2019-04-30 23:55:37 +0600

    UserController is final

--- src/main/java/com/hungry/controllers/UserController.java
+++ src/main/java/com/hungry/controllers/UserController.java
@@ -1,8 +1,5 @@
 package com.hungry.controllers;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -14,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,11 +23,9 @@ import org.springframework.web.multipart.MultipartFile;
 import com.hungry.entities.AccessToken;
 import com.hungry.entities.User;
 import com.hungry.models.Profile;
-import com.hungry.repositories.UserRepository;
 import com.hungry.services.UserService;
 import com.hungry.services.util.ConsumeType;
 import com.hungry.services.util.MultipartFileStoreService;
-import com.hungry.services.util.SecurityMaster;
 
 @Controller("userController")
 @RequestMapping(value = "/user")
@@ -43,26 +37,6 @@ public class UserController {
 	public UserService userService;
 	@Autowired
 	private MultipartFileStoreService MultipartFileStoreService;
-
-	@Autowired
-	private UserRepository userRepository;
-
-	@Autowired
-	private SecurityMaster securityMaster;
-
-	@GetMapping(value = "/debug")
-	public @ResponseBody ResponseEntity<AccessToken> debugger() {
-
-		try {
-			System.out.println(URLEncoder.encode("UKIR91811Ik2XUM0YwCKpiNW41Y0hOShQ8lTcyLbwCTUHFvDKGCVfRwNfT6xfbxSv7cP8pXOOdVFiMxmeN4PBFQcdgLW9I+yLIrnVL5XugKqjHQdb1lnydN8mNXD+R+ssbN9MIVzFQFYuZK+aicYKbw9vvu/a9gj/7Zh4D/Xtd6Sv+G46Cm/9kipQP+vj4tppbnOmWi1gwX4rVbhXwZTjF36s9Zna0LSKWBPUaHLEnYySS545P0dnB3fH+54X5AVdQyU0+10u4HuA5brI0FXBZXt53rF9yw==YTG8057",
-					StandardCharsets.UTF_8.toString()));
-		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return userService.debug();
-	}
 
 	@PostMapping(value = "/registration", consumes = { ConsumeType.JOSN })
 	public @ResponseBody ResponseEntity<AccessToken> isRegistrationComplete(@RequestBody User user) {
@@ -92,8 +66,4 @@ public class UserController {
 	public @ResponseBody ResponseEntity<Profile> profile(@RequestParam("token") String token) {
 		return userService.profile(token);
 	}
-	
-	
-	
-
 }
