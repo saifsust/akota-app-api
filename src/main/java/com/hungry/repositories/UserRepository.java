9ce61f89629325b0a4a133d75f5c4f66a3cfaf90
saifsust <saiful.sust.cse@gmail.com>
2019-04-30 20:45:38 +0600

    UserRepository updated

--- src/main/java/com/hungry/repositories/UserRepository.java
+++ src/main/java/com/hungry/repositories/UserRepository.java
@@ -9,9 +9,19 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.hungry.entities.User;
+import com.hungry.models.Profile;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+	/**
+	 * @param user_id receive a phone number and search hungry_users table to get
+	 *                user info.
+	 * @return user if find correspondanding user
+	 */
+
+	@Query(value = "SELECT * FROM hungry_users WHERE user_id = :user_id", nativeQuery = true)
+	public User findUserByUserId(@Param("user_id") int userId);
 
 	/**
 	 * @param phone receive a phone number and search hungry_users table to get user
@@ -31,20 +41,28 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 	/**
 	 * @return maximum id from hungry_users table
 	 */
-	///SELECT  ifnull((select MAX(user_id) as user_id FROM hungry_users),0)
-	
-	//@Query(value = "SELECT MAX(user_id) as user_id FROM hungry_users", nativeQuery = true)
 	@Query(value = "SELECT  IFNULL((SELECT MAX(user_id) as user_id FROM hungry_users),0)", nativeQuery = true)
 	public int findMaxUserId();
 
 	/**
-	 * update or insert user profile picture 
+	 * update or insert user profile picture
+	 * 
 	 * @param imgUrl is the image link
 	 * @param userId is the user id
 	 */
 	@Transactional
 	@Modifying
-	@Query(value = "UPDATE hungry_users SET hungry_users.user_img = :img WHERE hungry_users.user_id= :id", nativeQuery = true)
-	public void updateUserImage(@Param("img") String imgUrl, @Param("id") int userId);
+	@Query(value = "UPDATE hungry_users SET hungry_users.user_img = :img ,hungry_users.user_img_location = :user_img_location  WHERE hungry_users.user_id= :id", nativeQuery = true)
+	public void updateUserImage(@Param("img") String imgUrl, @Param("user_img_location") String user_img_location,
+			@Param("id") int userId);
+
+	/**
+	 * return user_img_location to delete or modify the user image
+	 * 
+	 * @param userId user id
+	 * @return user image local location
+	 */
+	@Query(value = "SELECT user_img_location FROM hungry_users WHERE hungry_users.user_id= :user_id", nativeQuery = true)
+	public String findImageLocalAddresss(@Param("user_id") int userId);
 
 }
