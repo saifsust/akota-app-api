c4770f8d60247ca48248004296f6e405027e3037
saifsust <saiful.sust.cse@gmail.com>
2019-05-01 01:43:04 +0600

    ProductRepository is imp

--- src/main/java/com/hungry/repositories/ProductRepository.java
+++ src/main/java/com/hungry/repositories/ProductRepository.java
@@ -1,25 +1,31 @@
 package com.hungry.repositories;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.hungry.entities.Product;
 
 @Repository
-public class ProductRepository {
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 
-	
-	@PersistenceContext
-	private EntityManager em;
-	
-	
-	@Transactional
-	public void persist(Product product) throws Exception {
-		em.persist(product);
-	}
-	
-	
+	@Query(value = "SELECT * FROM hungry_products WHERE product_name = :product_name", nativeQuery = true)
+	public List<Product> findProductsByName(@Param("product_name") String productName);
+
+	@Query(value = "SELECT * FROM hungry_products WHERE product_id = :product_id", nativeQuery = true)
+	public Product findProductById(@Param("product_id") int productId);
+
+	@Query(value = "SELECT * FROM hungry_products WHERE product_type = :product_type", nativeQuery = true)
+	public List<Product> findProductsByType(@Param("product_type") String productType);
+
+	@Query(value = "SELECT * FROM hungry_products WHERE product_type = :product_type and rating >= :rating", nativeQuery = true)
+	public List<Product> findProductsByRatingAndType(@Param("product_type") String productType,
+			@Param("rating") double rating);
+
+	@Query(value = "SELECT * FROM hungry_products WHERE product_name = :product_name and rating >= :rating", nativeQuery = true)
+	public List<Product> findProductsByRatingAndName(@Param("product_name") String productName,
+			@Param("rating") double rating);
 }
