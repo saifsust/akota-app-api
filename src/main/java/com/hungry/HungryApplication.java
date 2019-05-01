4752348bab171165658298bf5147786b42865223
saifsust <saiful.sust.cse@gmail.com>
2019-05-01 14:42:10 +0600

    Respository scannner add

--- src/main/java/com/hungry/HungryApplication.java
+++ src/main/java/com/hungry/HungryApplication.java
@@ -4,15 +4,15 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
-import org.hibernate.validator.internal.util.privilegedactions.GetResource;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -27,41 +27,35 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableAutoConfiguration
 @ComponentScan(basePackages = { "com.hungry.*" })
 @EntityScan(basePackages = { "com.hungry.entities" })
+@EnableJpaRepositories(basePackages = { "com.hungry.repositories" })
 @EnableTransactionManagement
-public class HungryApplication implements WebMvcConfigurer  {
-	
+public class HungryApplication implements WebMvcConfigurer {
+
 	public static void main(String[] args) {
 		SpringApplication.run(HungryApplication.class, args);
 	}
-	
-	
+
 	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {		
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/**").addResourceLocations("file:/media/saif-sust/WEB_project/hungry/images/");
 	}
-	
-	
-	
-    
-	
-	
 
 	@Bean(name = "entityManagerFactory")
 	public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
 		LocalContainerEntityManagerFactoryBean localContrainerEntityManager = new LocalContainerEntityManagerFactoryBean();
 		localContrainerEntityManager.setDataSource(dataSource);
-		localContrainerEntityManager.setPackagesToScan(new String[] { "com.hungry.entities" });
+		localContrainerEntityManager.setPackagesToScan(new String[] { "com.hungry.entities"});
 		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
 		localContrainerEntityManager.setJpaVendorAdapter(vendorAdapter);
 		return localContrainerEntityManager;
 	}
 
-	@Bean(name="entityManager")
+	@Bean(name = "entityManager")
 	public EntityManager getEntityManager(EntityManagerFactory entityManagerFactory) {
 		return entityManagerFactory.createEntityManager();
 	}
 
-	@Bean(name="transactionManager")
+	@Bean(name = "transactionManager")
 	public PlatformTransactionManager getPlatformTransactionManager(EntityManagerFactory entityManagerFactory) {
 		JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
 		jpaTransactionManager.setEntityManagerFactory(entityManagerFactory);
