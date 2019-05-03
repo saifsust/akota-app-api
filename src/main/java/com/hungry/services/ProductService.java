0eca2cbb30d4c06e1021db9ccd24513815f33e4c
saifsust <saiful.sust.cse@gmail.com>
2019-05-03 20:45:06 +0600

    merger with product

--- src/main/java/com/hungry/services/ProductService.java
+++ src/main/java/com/hungry/services/ProductService.java
@@ -2,7 +2,6 @@ package com.hungry.services;
 
 import java.util.List;
 
-import org.json.JSONArray;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
