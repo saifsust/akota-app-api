5ebc19ac5952f1a2c66658002b29c65d5c013656
saifsust <saiful.sust.cse@gmail.com>
2019-05-13 16:40:50 +0600

    receive order is partially impl

--- src/main/java/com/hungry/services/util/ConsumeType.java
+++
@@ -1,8 +0,0 @@
-package com.hungry.services.util;
-
-public interface ConsumeType {
-
-	public static final String JOSN = "application/json";
-	public static final String MULTIPART_FORM_DATA = "multipart/form-data";
-
-}
