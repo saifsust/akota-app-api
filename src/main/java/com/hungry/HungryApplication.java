da6d5b44659ad21509e9d4babe497cc03afe3959
saifsust <saiful.sust.cse@gmail.com>
2019-05-17 09:12:07 +0600

    pull from order

--- src/main/java/com/hungry/HungryApplication.java
+++ src/main/java/com/hungry/HungryApplication.java
@@ -1,8 +1,5 @@
 package com.hungry;
 
-import java.util.Arrays;
-import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
