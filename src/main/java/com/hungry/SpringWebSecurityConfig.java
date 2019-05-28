3c99fade9656f732d886d5cc77b92eca6d7ec471
saifsust <saiful.sust.cse@gmail.com>
2019-05-28 15:34:45 +0600

    clean conflict

--- src/main/java/com/hungry/SpringWebSecurityConfig.java
+++ src/main/java/com/hungry/SpringWebSecurityConfig.java
@@ -2,6 +2,8 @@ package com.hungry;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -18,40 +20,31 @@ import com.hungry.services.util.CryptoMaster;
 @Component
 public class SpringWebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	private static final Logger LOG = (Logger) LoggerFactory.getLogger(SpringWebSecurityConfig.class);
+
 	@Autowired
 	private UserRepository userRepository;
 	@Autowired
 	private CryptoMaster cryptoMaster;
 
-
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		List<User> users = userRepository.findAll();
 		for (User user : users) {
-			System.out.print(user);
+			LOG.debug(user.toString());
 			auth.inMemoryAuthentication().withUser(user.getPhone())
 					.password("{noop}" + cryptoMaster.decrypt(user.getPassword())).roles("USER");
 
 		}
-		// auth.authenticationProvider(tokenAuthenticationProvider);
 
 	}
 
 	// Secure the endpoins with HTTP Basic authentication
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.httpBasic()
-		.and()
-		.authorizeRequests()
-		.antMatchers(HttpMethod.GET, "/user/**").hasRole("USER")
-		.antMatchers(HttpMethod.POST,"/order").hasRole("USER")
-		.antMatchers(HttpMethod.POST,"/receiver").hasRole("USER")
-		.and()
-        .csrf().disable()
-        .formLogin().disable();
-		
+		http.httpBasic().and().authorizeRequests().antMatchers(HttpMethod.GET, "/user/**").hasRole("USER")
+				.antMatchers(HttpMethod.POST, "/order").hasRole("USER").antMatchers(HttpMethod.POST, "/receiver")
+				.hasRole("USER").and().csrf().disable().formLogin().disable();
+
 	}
-
-	
-
 
 }
